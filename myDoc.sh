b995c330f481481e3d95ed9957bdbdf6d1e6485b
pingwang326 <pingwang326@yahoo.com>
2018-09-20 11:56:42 -0400

    update run

--- myDoc.sh
+++ myDoc.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-openssl aes-256-cbc -salt -a -e -in reference/d-90Days -out reference/e-90Days -k wp
-openssl aes-256-cbc -salt -a -d -in reference/e-90Days -out reference/d-90Days -k wp
+MYSELF=`readlink -f $0`
+FQDN=`hostname -f`
+DIR=`dirname $0`
+FMT="%Y-%m-%d %H:%M:%S"
+NOW=$( date +"${FMT}" )
 
-openssl aes-256-cbc -salt -a -e -in reference/d-personal -out reference/e-personal -k wp
-openssl aes-256-cbc -salt -a -d -in reference/e-personal -out reference/d-personal -k wp
+cd
+pwd=$(cat key/.test)
+
+cd $DIR
+openssl aes-256-cbc -salt -a -e -in reference/d-90Days -out reference/e-90Days -k $pwd
+openssl aes-256-cbc -salt -a -d -in reference/e-90Days -out reference/d-90Days -k $pwd
+
+openssl aes-256-cbc -salt -a -e -in reference/d-personal -out reference/e-personal -k $pwd
+openssl aes-256-cbc -salt -a -d -in reference/e-personal -out reference/d-personal -k $pwd
