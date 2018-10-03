b597565e99893d0221d3c1b659706050d7c52072
wangpin <wangpin@us.ibm.com>
2018-10-03 16:47:57 -0400

    U

--- myDoc.sh
+++ myDoc.sh
@@ -11,12 +11,14 @@ cd $DIR
 
 if [ $1 == "out" ];then
   echo $1
+  rm reference/e-*
   openssl aes-256-cbc -salt -a -e -in reference/d-90Days -out reference/e-90Days -k $pwd
   openssl aes-256-cbc -salt -a -e -in reference/d-personal -out reference/e-personal -k $pwd
 fi
 
 if [ $1 == "in" ]; then
   echo $1
+  rm reference/d-*
   openssl aes-256-cbc -salt -a -d -in reference/e-90Days -out reference/d-90Days -k $pwd
   openssl aes-256-cbc -salt -a -d -in reference/e-personal -out reference/d-personal -k $pwd
 fi
